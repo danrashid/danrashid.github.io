97c6189879780286c7ca587af1a52821044230ae
Dan Rashid <dan@pecsafety.com>
2020-02-01 22:09:12 -0600

    Add timeline

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var timelineWidth = -1;
+var timelineScale = 30;
+
+document
+  .querySelectorAll("#timeline > div:first-child > div")
+  .forEach(function(div) {
+    timelineWidth += div.dataset.months * timelineScale;
+  });
+
+document.getElementById("timeline").style.width = timelineWidth + "px";
+
+document.querySelectorAll("#timeline > div > div").forEach(function(div) {
+  div.style.width = div.dataset.months * timelineScale - 1 + "px";
+});
